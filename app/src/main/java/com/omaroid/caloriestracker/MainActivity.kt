b1f5621d01391d7896922bc91eb964a5fd9611f1
Omar <andomaroid1@gmail.com>
2024-01-24 12:55:20 +0100

    no message

--- app/src/main/java/com/omaroid/caloriestracker/MainActivity.kt
+++ app/src/main/java/com/omaroid/caloriestracker/MainActivity.kt
@@ -5,15 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.omaroid.caloriestracker.ui.theme.CaloriesTrackerTheme
 import com.omaroid.onboarding_presentation.welcome.WelcomeScreen
-import com.omaroid.onboarding_presentation.welcome.WelcomeScreenPreview
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
