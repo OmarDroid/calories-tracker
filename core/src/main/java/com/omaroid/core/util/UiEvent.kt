3006ea9278249c6a5ed0c584dd74ccd35bab0435
Omar <andomaroid1@gmail.com>
2024-01-26 19:21:51 +0100

    Implement needed helper classes and business logic
    * Implement UiText.kt
    * Add ShowSnackBar to UiEvent.kt that takes UiText as a param
    * Implement FilterOutDigits.kt use case
    * Add FilterOutDigits to DI
    
    Signed-off-by: Omar <andomaroid1@gmail.com>

--- core/src/main/java/com/omaroid/core/util/UiEvent.kt
+++ core/src/main/java/com/omaroid/core/util/UiEvent.kt
@@ -4,4 +4,5 @@ sealed class UiEvent {
     data object Success : UiEvent()
     data class Navigate(val route: String): UiEvent()
     data object NavigateUp: UiEvent()
+    data class ShowSnackBar(val message: UiText) : UiEvent()
 }
