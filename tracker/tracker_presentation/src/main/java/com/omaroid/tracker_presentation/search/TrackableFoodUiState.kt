a28668011ee006e8817c9e703cc2c5843c46390a
Omar <andomaroid1@gmail.com>
2024-02-21 12:32:42 +0100

    Implement Search ViewModel:
    * Implement UiState
    * Implement Event
    * Implement ViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/omaroid/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.omaroid.tracker_presentation.search
+
+import com.omaroid.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
